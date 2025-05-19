5cd3083eff03460b2ea0bfdb6e8d64c323d331b7
MitKotadiya <kotadiyamit17552@gmail.com>
2025-05-19 16:25:39 +0530

    all file commite

---
+++ programe.js
@@ -0,0 +1,116 @@
+// 1...> WRITE A JAVASCRIPT PROGRAM TO FIND THE AREA OF A TRIANGLE.
+
+let area , base=8 , height=10;
+area = 0.5 * base * height;
+
+console.log("Base of the triangle is:", base);
+console.log("Height of the triangle is:", height);
+console.log("Area of the triangle is:", area);
+
+
+// 2...> WRITE A JAVASCRIPT PROGRAM TO CONVERT TEMPERATURES TO AND FROM CELSIUS, FAHRENHEIT. 
+
+// let celsius = 40 , fahrenheit;
+
+// fahrenheit = (celsius * 9/5) + 32;
+
+// console.log(celsius + "°C =",  + fahrenheit ,"F");
+
+
+// 3...> WRITE A JAVASCRIPT PROGRAM TO FIND THE AREA OF A RECTANGLE
+
+// let area , width=8 , length=10;
+
+// area =  width * length;
+
+// console.log("Width of the rectangle is:", width);
+// console.log("Length of the rectangle is:", length);
+// console.log("Area of the rectangle is:", area);
+
+
+// 4...> WRITE A JAVASCRIPT PROGRAM TO FIND THE AREA OF A CIRCLE
+
+// let  area , radius = 4 , pi=3.14;
+
+// area=pi*radius*radius;
+
+// console.log("Radius of the circle is:", radius);
+// console.log("Area of the circle is:", area);
+
+
+// 5...> WRITE A JAVASCRIPT PROGRAM TO CONVERT TO FEET INTO INCHES
+
+// let feet = 5 , inches;
+
+// inches = feet*12;
+
+// console.log("Feet :" ,feet)
+// console.log(feet + " feet =", inches + " inches");
+
+
+// 6...>WRITE A JAVASCRIPT PROGRAM TO CALCULATE GIVEN FORMULAS. 
+
+//  6.1...> a ^ 2 - b ^ 2 = (a - b)(a + b) 
+
+// let a=10, b=5 ,left ,right,ans;
+
+// left = (a-b) * (a+b);
+// right = a**2 - b**2;
+// ans = right == left;
+
+// console.log("Right side is :" ,right)
+// console.log("Left side is :" , left)
+// console.log("Its prove that R.H.S == L.H.S :" , ans)
+
+
+//  6.2...> (a - b) ^ 2 = a ^ 2 - 2ab + b ^ 2 
+
+//  let a=10, b=5 ,left , right , ans;
+
+//  left = (a-b)**2;
+//  right= a**2 - (2*a*b) + b**2;
+//  ans = right == left;
+
+//  console.log("Right side is :" ,right)
+//  console.log("Left side is :" , left)
+//  console.log("Its prove that R.H.S == L.H.S :" , ans)
+
+
+//  6.3...> (a + b + c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 + 2ab + 2ac + 2bc  
+
+//  let a=10, b=5 , c=2 , left , right , ans;
+
+//  left = (a + b + c)**2;
+//  right= a**2 + b**2 + c**2 + (2*a*b) + (2*a*c) + (2*b*c);
+//  ans = right == left;
+
+//  console.log("Right side is :" ,right)
+//  console.log("Left side is :" , left)
+//  console.log("Its prove that R.H.S == L.H.S :" , ans)
+
+
+//  6.4...> (a - b - c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 - 2ab - 2ac + 2bc 
+
+//  let a=10, b=5 , c=2 , left , right , ans;
+
+//  left = (a + b + c)**2;
+//  right= a**2 + b**2 + c**2 + (2*a*b) + (2*a*c) + (2*b*c);
+//  ans = right == left;
+
+//  console.log("Right side is :" ,right)
+//  console.log("Left side is :" , left)
+//  console.log("Its prove that R.H.S == L.H.S :" , ans)
+
+
+//  6.5...> (a - b) ^ 3 = a ^ 3 - 3a ^ 2 * b + 3a * b ^ 2 - b ^ 3 
+
+
+//  let a=10, b=5 ,left , right , ans;
+
+//  left = (a - b)**3;
+//  right= a**3 - (3*a**2*b) + (3*a*b**2) - b**3;
+//  ans = right == left;
+
+//  console.log("Right side is :" ,right)
+//  console.log("Left side is :" , left)
+//  console.log("Its prove that R.H.S == L.H.S :" , ans)
